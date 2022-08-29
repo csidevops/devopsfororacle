19ec0df610b612c9f955f80c49c545f06d0d6180
csidevops <56381436+csidevops@users.noreply.github.com>
2022-08-29 15:25:05 +0530

    Update altercustmastdb.sql

--- scripts/altercustmastdb.sql
+++ scripts/altercustmastdb.sql
@@ -7,7 +7,7 @@ ADD (POSTAL_CODE CHAR(10) );
 
 -- liquibase formatted sql
 -- changeset csidevops:20220819-005 
-INSERT INTO CUSTOMER_MASTER_DATA VALUES(DB_SEQUENCE.nextval, 'James', 'Anderson', '17-20 Arlington St', 'St. James's, 'London', NULL, 'UK', 5612347890, 8901256734, 'NW1 4NP');
+INSERT INTO CUSTOMER_MASTER_DATA VALUES(DB_SEQUENCE.nextval, 'James', 'Anderson', '17-20 Arlington St', 'APT # 1', 'London', NULL, 'UK', 5612347890, 8901256734, 'NW1 4NP');
 INSERT INTO CUSTOMER_MASTER_DATA VALUES(DB_SEQUENCE.nextval, 'John', 'Krasinski', '3E2 Jains Inseli Park', 'APT # 2', 'Chennai', NULL, 'INDIA', 5678912340, 3456012789, '603103');
 
 -- rollback CREATE TABLE CUSTOMER_MASTER_DATA_BACKUP AS SELECT * FROM CUSTOMER_MASTER_DATA;
